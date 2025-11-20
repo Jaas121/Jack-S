a495a0c7edab9dbebabd717793626cfc167d131b
jAlexander <jackslaughter34@gmail.com>
2025-11-21 12:17:04 +1300

    Started correction
    Almost there, issues with implementing the 2 asset variance formula, it's there but commented out. I also know that it's slow and could reduced amount of time data is downloaded.

--- sharp_ratio_heatmap.py
+++ sharp_ratio_heatmap.py
@@ -15,22 +15,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 import yfinance as yf
 from datetime import datetime,timedelta
+import pandas as pd
 
-# 10 rows and 10 columns of random data
-# data = np.random.rand(10,10)
+
 
 ticker1 = 'AAPL'
 ticker2 = 'MSFT'
 
 
-
-def expected_return(ticker,lookback):
-    """ Yfinance does not have an expected return function, so we will the mean of past returns, over the last 2 years """
+def get_daily_returns(ticker,lookback):
+    """ Returns daily returns, called for expected_retur function and for finding correlation (rho) """
     end = datetime.today()
     start = end - timedelta(lookback)
     # Progress = False, gets rid of the progress bar which is automaticly printed
     price_data = yf.download(ticker,start=start,end=end,progress=False).dropna()
-    daily_returns = price_data['Close'].pct_change().dropna()
+
+    return price_data['Close'].pct_change().dropna()
+
+
+def expected_return(ticker,lookback):
+    """ Yfinance does not have an expected return function, so we will find the mean of past returns, over the last 2 years """
+    daily_returns = get_daily_returns(ticker,lookback)
     ave_annual_returns = daily_returns.mean()*252
 
     return ave_annual_returns
@@ -46,7 +51,7 @@ def risk_free_rate():
 
 
 def std_deviation(ticker,lookback):
-    """ Getting std deviation which represents the volitilty of the portfolio, over last 2 years """
+    """ Getting std deviation which represents the volitilty of the portfolio"""
     end = datetime.today()
     start = end - timedelta(lookback)
     price_data = yf.download(ticker,start=start,end=end,progress=False)
@@ -67,7 +72,7 @@ def get_sharp_matrix():
     WEIGHT_STEPS = 10
 
     # rf constant
-    rf = risk_free_rate().iloc[0]
+    rf = risk_free_rate()
 
     for lb in range(1,11):
         lookback = lb * LOOKBACK_STEP
@@ -83,7 +88,19 @@ def get_sharp_matrix():
             weight2 = 1 - weight1
 
             portfolio_return = exp1*weight1 + exp2*weight2
+            # Now taking into account the correlation of the 2 assets
+
+            # daily_returns_ticker1 = get_daily_returns(ticker1,lookback)
+            # print(f"daily_returns_ticker1: {daily_returns_ticker1}")
+            # daily_returns_ticker2 = get_daily_returns(ticker2,lookback)
+            # print(f"daily_returns_ticker2: {daily_returns_ticker2}")
+            # correlation = daily_returns_ticker1.corr(daily_returns_ticker2)
+
+            # portfolio_std = np.sqrt(std1**2*weight1**2 + std2**2*weight2**2 + 2*weight1*weight2*std1*std2*correlation)
+
             portfolio_std = std1*weight1 + std2*weight2
+
+
 
             sharp = (portfolio_return - rf) / portfolio_std
 
