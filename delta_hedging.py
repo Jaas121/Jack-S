d2002544ec78bab68972bba55acbf7ddb3050517
jAlexander <jackslaughter34@gmail.com>
2025-11-26 16:55:39 +1300

    Updated delta hedging project
    Had to change a lot around, again it was mostly the conceptual side I struggled with, didn't need the hedge ratio function. Added knowledge that 100 shares typically make up a contract. Need more user output.

--- delta_hedging.py
+++ delta_hedging.py
@@ -2,10 +2,8 @@
 Author: Jack Slaughter
 Date: 25/11/25
 
-
 Delta hedging is ensuring portfolio is delta-neutral, (Delta is sensitivity of an options price to the underlying asset price),
 it's important as it hedges portfolio against changes in the underlying asset price.
-
 
 Considerations:
     Ensuring portfolio is not over rebalanced, due transaction.
@@ -13,56 +11,84 @@ Considerations:
     External market conditions.
 """
 
+import math
+import numpy as np
 
-from Black_scholes import call_black_scholes,cumulative_dist
-
-
-# Constant ensuring portfolio is not over rebalanced
+# Ensure portfolio is not over rebalanced
 LEEWAY = 0.25
+
 s = 100
 k = 75
-t = 5
+t = 0.5
 sigma = 0.1
 r = 0.2
 
+# How many option contracts held, & how many stocks make up an option 
+contracts = 10 
+shares_per_contract = 100 
+position_type = 'short call'  
 
 
-def black_scholes_delta(s,k,t,sigma,r,option_type='call'):
+def cumulative_dist(d):
+    """ Cumulative dist function for a standard normal distribution, 
+    the intergral doesn't have a simple algebraic solution, so use error function for an aproximation """
+    return 0.5*(1+math.erf(d/np.sqrt(2)))
+
+def bs_call_delta(S,K,T,sigma,r):
+    d1 = (np.log(S/K) + (r + (sigma**2/2))*T)/(sigma * np.sqrt(T))
+    d2 = d1 - (sigma * np.sqrt(T))
+    c = S * cumulative_dist(d1) - np.exp(-r*T) * K * cumulative_dist(d2)
+    return c
+
+def bs_put_delta(S,K,T,sigma,r):
+    return bs_call_delta(S,K,T,sigma,r)-1
+
+
+def option_delta(s,k,t,sigma,r,position_type):
     """ Getting delta with the black scholes """
-
     # Adjust for call or put
-    if option_type=='call':
-        return call_black_scholes(s,k,t,sigma,r)
+    if 'call' in position_type:
+        delta_per_share = bs_call_delta(s,k,t,sigma,r)
+    elif 'put' in position_type:
+        delta_per_share =  bs_put_delta(s,k,t,sigma,r)
     else:
-        return call_black_scholes(s,k,t,sigma,r)-1
-    
-
-
-print(black_scholes_delta(s,k,t,sigma,r))
-print(black_scholes_delta(s,k,t,sigma,r,option_type='put'))
+        return ValueError("Option_type must be either call or put")
+    sign = -1 if 'short' in position_type else 1
+    return sign * delta_per_share
 
 
 
-def hedge_ratio(hedge_position,exposure):
-    """ Important in showing how much protection you're getting """
-    return hedge_position/exposure
 
+def get_portfolio_delta():
+    """ w """
+    delta_per_contract = option_delta(s,k,t,sigma,r,position_type)
+    total_delta_contracts = delta_per_contract*contracts
+    total_delta_shares = total_delta_contracts*shares_per_contract
+    return ({
+    'per_contract':delta_per_contract,
+    'total_contracts':total_delta_contracts,
+    'total_shares':total_delta_shares})
 
 
 def adjusting_hedge():
-    """ Whenever delta moves from zero, executes buying or selling trade to reestablish delta neutrality """
-    current_delta = black_scholes_delta(s,k,t,sigma,r)
+    """ Whenever delta moves from zero, executes buying or selling trade to re-establish delta neutrality 
+    note: inlude option type
+    """
+    portfolio_delta = get_portfolio_delta()
+    total_shares_delta = portfolio_delta['total_shares']
+    tolerance_shares = LEEWAY * shares_per_contract
 
-    # Within threshold
-    if abs(current_delta) <= LEEWAY:
+
+    if abs(total_shares_delta) <= tolerance_shares:
+        print(f"Within threshold ({tolerance_shares}) no trade needed")
         return
 
-    # Delta too low ---> buy
-    if current_delta < -LEEWAY:
-        # delta negitive, buy trade executed to get back to delta-neutral 
-        diference = (current_delta)-LEEWAY
+    # Go in the opposite direction 
+    shares_to_cancel = -total_shares_delta
 
-    # Not too low & not in leeway --> too high --> sell
-    else:
-        # delta positive, sell trade executed to get back to delta-neutral
-        diference = current_delta-LEEWAY
+    action = 'Buy' if shares_to_cancel<0 else 'Sell'
+    print(f"{action} {abs(round(shares_to_cancel))} to be delta neutral")
+    
+
+
+adjusting_hedge()
